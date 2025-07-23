4b41484304fd55488012191acd38a70a1ab022f3
arashiviv <140507485+arashiviv@users.noreply.github.com>
2025-07-23 10:41:09 +0300

    init comit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import ToDoList from './components/ToDoList.vue'
+import { ref } from 'vue'
+
+const todos = ref([
+  { title: 'Lorean Ipsum1', isDone: false },
+  { title: 'Lorean Ipsum2', isDone: false },
+  { title: 'Lorean Ipsum3', isDone: false },
+])
+const markTodoAsComplete = (index) => {
+  todos.value[index].isDone = !todos.value[index].isDone
+}
+</script>
+
+<template>
+  <div id="todo-page">
+    <div id="todo">
+      <header>GGIT ToDo</header>
+
+      <ToDoList :todos="todos" @complete-todo="markTodoAsComplete" />
+
+      <button>+ New Task</button>
+    </div>
+  </div>
+</template>
+<style scoped>
+header {
+  background-color: blueviolet;
+  color: aliceblue;
+  padding: 24px;
+  font-weight: 600;
+}
+#todo {
+  width: 100%;
+  max-width: 250px;
+}
+#todo-page {
+  min-height: 100vh;
+  min-width: 100vw;
+  overflow: hidden;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: aliceblue;
+}
+#todos-container {
+  background-color: white;
+  padding: 24px;
+  margin-top: 24px;
+}
+button {
+  color: white;
+  background-color: blue;
+  padding: 12px 16px;
+  font-size: 18 px;
+  border: none;
+  border-radius: 50px;
+  margin: auto;
+  margin-top: -22px;
+  display: block;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<!-- <template>
+  <div>
+    <h1>Hello, World!</h1>
+
+    <h2>{{ counter }}</h2>
+
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref, onUpdated, computed ,watch} from 'vue'
+
+const counter = ref(0);
+
+const increase = () => {
+    const result = computed ( ) => {
+        return 'Counter New value : >>>' + counter,value
+    }
+    watch (counter ,() =>{
+     consule 'Counter New value : >>>", COUNTE)
+    }
+  counter.value += 1
+}
+
+const decrease = () => {
+  counter.value -= 1
+}
+
+onMounted(() => {
+  console.log('Componentul a fost montat')
+})
+
+onUpdated(() => {
+  console.log('Re rerender')
+})
+</script>
+
+<style scoped></style> -->
